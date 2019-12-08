62953aeeb156215e317d434bd632780099be1526
CodeMonkey <dmsqlctnekf@gmail.com>
2019-12-08 22:24:07 +0900

    'test'

--- Intellij_Test/src/main/java/com/spring/mapper/MemberMapper.java
+++
@@ -1,9 +0,0 @@
-package com.spring.mapper;
-
-import com.spring.dto.MemberDTO;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface MemberMapper {
-    MemberDTO selectMember(String id);
-}
--- Intellij_Test/src/main/java/legacy/Index.java
+++
@@ -1,4 +0,0 @@
-package legacy;
-
-public class Index {
-}
